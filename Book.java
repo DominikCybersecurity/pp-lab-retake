a5d9b86723563cabb8cb5df8a7f2c547c5103fe2
Dominik Sulima <166212630+DominikCybersecurity@users.noreply.github.com>
2024-06-02 11:58:11 +0200

    Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,29 @@
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private int year;
+
+    public Book(String title, String author, String isbn, int year) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.year = year;
+    }
+
+    // Getters and Setters
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public int getYear() { return year; }
+    public void setYear(int year) { this.year = year; }
+
 
